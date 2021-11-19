bca24424fd98e65800c3ab62f3dfd9a5e3eb3eac
jberbissbd <jordi.berbis.11@gmail.com>
2021-11-19 20:14:54 +0100

    inicialització creada

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from datetime import date
 import pandas.errors
 import csv
 
-alumnat = '/dades/alumnat.csv'
+alumnat = 'dades/alumnat.csv'
 categories = 'dades/categories.csv'
+arxiubbdd = "dades/registre.db"
 
 al_seguiment = ""
 cat_seguiment = ""
@@ -13,6 +14,7 @@ cat_seguiment = ""
 
 def lectura_dades():
     global alumnat
+    global categories
     global al_seguiment
     global cat_seguiment
     try:
@@ -29,13 +31,13 @@ def lectura_dades():
         print("Sense dades d'alumnes, no es pot seguir")
 
     try:
-        with open("categories.csv") as file:
+        with open(categories) as file:
             cat_seguiment = pd.read_csv(file)
             file.close()
     except FileNotFoundError:
         print("Llistat de categories buit, es torna a crear")
         motius_llista = ["Motius"]
-        with open("categories.csv", "w") as file:
+        with open(categories, "w") as file:
             writer = csv.writer(file)
             writer.writerow(motius_llista)
     except pandas.errors.EmptyDataError:
@@ -48,6 +50,7 @@ def bbdd_conn():
     try:
         conn = sqlite3.connect(arxiubbdd)
         conn.cursor()
+        conn.close()
 
     except sqlite3.OperationalError:
         print("error")
@@ -55,10 +58,10 @@ def bbdd_conn():
     # conn.close()
 
 
+lectura_dades()
+bbdd_conn()
 avui_real = date.today()
 avui_format = avui_real.strftime("%d/%m/%Y")
-bbdd_conn()
-print(avui_format)
-lectura_dades()
-print(al_seguiment)
-print(cat_seguiment)
+# print(avui_format)
+# print(al_seguiment)
+# print(cat_seguiment)
