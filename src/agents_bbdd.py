18672a748cfbc25b1231eef1ccf7fd1dba5c365e
jberbissbd <jordi.berbis.11@gmail.com>
2022-08-19 14:02:12 +0200

    Comprovant problemes pel que fa a la introduccio de valors llegits del config.ini

--- src/agents_bbdd.py
+++ src/agents_bbdd.py
@@ -81,8 +81,10 @@ class Liquidador(ModelDao):
 def obtenir_valors_categories():
     """Metode per a parsejar les categories del config.ini"""
     config = configparser.ConfigParser()
+    print(obtenir_ruta_config())
     config.read(obtenir_ruta_config())
     valors = config.get('Categories', 'defecte').split(', ')
+    print(valors)
     return valors
 
 
