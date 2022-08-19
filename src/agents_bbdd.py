1efbc6c801560b09a6c5ec658f39013a945ad372
jberbissbd <jordi.berbis.11@gmail.com>
2022-08-19 11:54:45 +0200

    Prova per a diagnosticar el problema.

--- src/agents_bbdd.py
+++ src/agents_bbdd.py
@@ -43,6 +43,7 @@ class AjudantDirectoris:
         ruta = None
         if self.mode == 1:
             localitzacio_bbdd = os.path.normpath(os.path.join(directori_arrel, "dades", "registre.db"))
+            print(localitzacio_bbdd)
             ruta = os.path.abspath(localitzacio_bbdd)
         elif self.mode == 2:
             localitzacio_bbdd = os.path.normpath(os.path.join(directori_arrel), "tests", "tests.db")
