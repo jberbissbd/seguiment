4d1f7f14cd50ea088c9aec482dc9f3f4bcb0a74e
jberbissbd <jordi.berbis.11@gmail.com>
2022-08-19 10:13:54 +0200

    Canvis en els noms dels arxius de sortida i en els agents_bbdd.py i agents_gui.py per a especificar les rutes.

--- src/agents/agents_bbdd.py
+++ src/agents/agents_bbdd.py
@@ -7,7 +7,7 @@ import sys
 # sys.path.append(os.path.abspath(dirname(__file__)))
 sys.path.append(os.path.normpath(os.path.dirname(os.path.abspath(__file__))))
 import configparser
-from formats import RegistresBbddComm, Registres_bbdd_nou, CategoriaComm, Alumne_comm, \
+from src.agents.formats import RegistresBbddComm, Registres_bbdd_nou, CategoriaComm, Alumne_comm, \
     DataGuiComm, DataNova, AlumneNou
 
 ERROR_LLISTA = "Error: el missatge ha de ser una llista."
