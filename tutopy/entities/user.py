23d6b5fcc2f90e07cf6103c747c303c37177af38
jberbissbd <75541976+jberbissbd@users.noreply.github.com>
2026-06-13 09:18:41 +0200

    Afegides Classes Contact i StudentAnnotations per ampliar funcionalitats

--- tutopy/entities/user.py
+++ tutopy/entities/user.py
@@ -108,3 +108,51 @@ class NoteRecord:
             datetime.date.fromisoformat(self.date)
         except ValueError:
             raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+
+
+@dataclass
+class Contact:
+    """Persones de contacte associats a l'aulmne. Familiars o altres professionals externs al centre.
+
+    Attributes:
+        id: Identificador únic.
+        student_id: Referència a l'alumne.
+        name: Nom de la persona
+        description: Descripció, per a descriure la relació amb l'alumne.
+        category_id: Referència a la categoria.
+        phone: Numero de telèfon de la persona de contacte.
+        email: Correu electrònic de la persona de contacte.
+    """
+    id: int
+    student_id: int
+    name: str
+    description: str
+    phone: str =""
+    email: str=""
+
+    def __post_init__(self):
+        for field in fields(self):
+            value = getattr(self, field.name)
+            if not isinstance(value, field.type):
+                raise ValueError(f'Expected {field.name} to be {field.type}, '
+                                f'got {repr(value)}')
+
+@dataclass
+class StudentAnnotation:
+    """Característiques o descriptores de l'alumne.
+
+    Attributes:
+        id: Identificador únic.
+        student_id: Referència a l'alumne.
+        content: Descripció.
+    """
+    id: int
+    student_id: int
+    content: str
+
+    def __post_init__(self):
+        for field in fields(self):
+            value = getattr(self, field.name)
+            if not isinstance(value, field.type):
+                raise ValueError(f'Expected {field.name} to be {field.type}, '
+                                f'got {repr(value)}')
