4d69d0c5109ba9353e90be3f100a4fcd7ee5ee41
jberbissbd <75541976+jberbissbd@users.noreply.github.com>
2026-06-20 21:28:08 +0200

    Afegides docstrings a test_dates.py

--- tests/utils/test_dates.py
+++ tests/utils/test_dates.py
@@ -1,12 +1,17 @@
 from tutopy.services.utils import DateConverter
 
 
-class Test_conversio():
+class TestConversio():
+    """
+    Tests per a comprovar la conversió de dates
+    """
 
     def test_isotouser(self, iso_date_example):
+        """Comprova que la conversió a format convencional es faci correctament"""
         conversor = DateConverter()
         assert conversor.iso_to_display(iso_date_example) == "01/01/2026"
 
     def test_usertoiso(self, user_date_example):
+        """Comprova que la conversió a format ISO es faci correctament"""
         conversor = DateConverter()
         assert conversor.display_to_iso(user_date_example) == "2026-01-01"
