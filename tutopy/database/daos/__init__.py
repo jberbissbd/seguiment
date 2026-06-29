6a36d566821b0713ef7196a8b9f36dc484ccd437
jberbissbd <75541976+jberbissbd@users.noreply.github.com>
2026-06-29 08:43:16 +0200

    Modificat __init__.py de daos

---
+++ tutopy/database/daos/__init__.py
@@ -0,0 +1,17 @@
+from .academic_course_dao import AcademicCourseDAO
+from .category_dao import CategoryDAO
+from .student_dao import StudentDAO
+from .note_dao import NoteDAO
+from .contact_dao import ContactDAO
+from .annotation_dao import AnnotationDAO
+from .document_dao import DocumentDAO
+
+__all__ = [
+    "AcademicCourseDAO",
+    "CategoryDAO",
+    "StudentDAO",
+    "NoteDAO",
+    "ContactDAO",
+    "AnnotationDAO",
+    "DocumentDAO",
+]
